2d33ef0e7af262107087db62e1e070325b73d583
Ravi varma <ravivarmar797@email.com>
2025-12-27 12:55:32 +0530

    MILESTONE 3: Designing & Storing Expense Data

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,3 +1,4 @@
+expenses = []
 running = True
 while running:
     print("\n------personal expense tracker-------")
@@ -8,7 +9,19 @@ while running:
     print("5.Exit")
     choice = input("Enter the choice(1-5)")
     if choice == "1":
-        print("add expense selected")
+        item = input("Enter item name: ")
+        amount = float(input("Enter amount: "))
+        category = input("Enter category: ")
+        date = input("Enter date (YYYY-MM-DD): ")
+
+        expense = {
+            "item": item,
+            "amount":amount,
+            "category":category,
+            "date":date
+        } 
+        expenses.append(expense)
+        print("Expense added successfully!")
     elif choice == "2":
         print("view expense selected")
     elif choice == "3":
@@ -21,4 +34,5 @@ while running:
     
     else:
         print("invalid choice. pls enter 1 to 5.")
-        
+
+
